c0d3b44fc2155c34f5bc6764790abc0de90079d5
Luke <knightfication@gmail.com>
2018-09-27 23:03:03 -0500

    Initial TicTacToe Build
    Added an app drawn board instead of an image just for tutorial following
    purposes.
    
    Pieces are just for show thus far.
    
    A few functions already included, although not yet tested:
    
    renderTile(): where tiles are decided and rendered
    componentDiDMount(): tests if game initialize or not
    initializeGame(): where the board pieces are saved (I will rename it
    later, just realized that the name isn't good on this one)

--- App.js
+++ App.js
@@ -1,25 +1,114 @@
+/*
+    I hate the lack of proper SOLID principle in this file.
+    Hopefully my JS will improve enough during this project so it won't
+    look like spaghetti code by the end. -Lucas
+*/
+
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Image
-            source = { require ("./assets/square.png") }
-            // style  = {{ width: 300, height: 300}}
-        />
-      </View>
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            gamestate: [[]],
+            currentPlayer: 1,
+        }
+    }
+
+    // Called when game initializes
+    componentDidMount() {
+        this.initializeGame();
+    }
+
+    // Where X and O selections are made
+    renderTile = (row, col) => {
+        var value = this.state.gameState[row][col];
+
+        switch(value)
+        {
+            case 1:
+                return <Image
+                    source = { require ("./assets/x.png") }
+                    style  = {{ width: 100, height: 100 }}
+                />
+            case 2:
+                return <Image
+                    source = { require ("./assets/o.png") }
+                    style  = {{ width: 100, height: 100 }}
+                />
+            default:
+                return <View />;
+        }
+    }
+
+    // Array that holds the values placed on the board
+    initializeGame = () => {
+        this.setState({ gamestate:
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+            ]
+        });
+    }
+
+    render() {
+        return (
+            <View style = {styles.container}>
+
+                <View style = {{ flexDirection: "row" }} >
+                    <View style = { [styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }] } >
+
+                        <Image
+                            source = { require ("./assets/x.png") }
+                            style  = {{ width: 75, height: 75 }}
+                        />
+
+                    </View>
+
+                    <View style = { [styles.tile, { borderTopWidth: 0 } ]} >
+
+                        <Image
+                            source = { require ("./assets/o.png") }
+                            style  = {{ width: 75, height: 75 }}
+                        />
+
+                    </View>
+                    <View style = { [styles.tile, { borderTopWidth: 0, borderRightWidth: 0} ]} />
+                </View>
+
+                <View style = {{ flexDirection: "row" }} >
+                    <View style = { [styles.tile, { borderLeftWidth: 0, } ]} />
+                    <View style = { [styles.tile, { } ]} />
+                    <View style = { [styles.tile, { borderRightWidth: 0, } ]} />
+                </View>
+
+                <View style = {{ flexDirection: "row" }} >
+                    <View style = { [styles.tile, { borderBottomWidth: 0, borderLeftWidth: 0, } ]} />
+                    <View style = { [styles.tile, { borderBottomWidth: 0, } ]} />
+                    <View style = { [styles.tile, { borderBottomWidth: 0, borderRightWidth: 0, } ]} />
+                </View>
+
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    tile: {
+        borderWidth: 10,
+        width: 100,
+        height: 100,
+    },
 });
