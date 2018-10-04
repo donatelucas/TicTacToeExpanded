f6ff5a74fea2cf33b8db42cee0968fbc1791b99a
donatelucas <dperez15@luc.edu>
2018-10-03 21:38:57 -0500

    Big changes
    We are ditching React Native for this project because we are pretty much learning Javascript, with the challenge in time, and schedules, learning it and React Native proved a challenge. Thus, we are ditching it to make the new version of the app in Javascript and some markup language, probably HTML+CSS, or some variant like Pug and Stylus.
    
    startGame(): calls the initialization of the game (also for replays), initializes the board, connecting the cells with the CSS and adds a click function to look for them. 
    
    function turnClick(): will be adding the players function.
    
    function turn(): will be adding turn controls.

---
+++ main.js
@@ -0,0 +1,35 @@
+var Board;
+const empty = '0';
+const x = '1';
+const o = '2';
+
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+	document.getElementById('.endgame').style.display = "none";
+	Board = Array.from(Array(9).keys());
+    console.log(Board);
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);
+    }
+}
+
+function turnClick(tile) {
+
+}
+
+function turn()
