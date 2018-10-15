eea38cae357111ed9bdc499a628d238ff31729ee
Luke <knightfication@gmail.com>
2018-10-14 21:33:05 -0500

    Huge Refactor - Please Read
    Since we still needed to add a way to get usernames, a refactor on how
    messages were displayed and the board was done was needed.
    
    Before, just the variable document.turn would do the job. Now, there are
    more variables controlling it, and a bunch of changes.
    
    The main one is that one of the main methods, turnControl(), was split
    in two. It became the observer() (as in the Observer Pattern, because
    that's what it is), and the resultDisplay(), which makes a little more
    elegant, SOLID principle, and distributes functionality better, besides
    solving the problem of the single variable for board and messages.
    
    I'm reopening the issue with Draws because I didn't adjust to display a
    drawing message with the refactor yet.

--- main.js
+++ main.js
@@ -1,29 +1,45 @@
+/*
+    This Javascript is rough. I was learning Javascript during this project,
+    actually, pretty much learned it just to make this project in Javascript, since
+    I always wanted to add a very used scripting language to my arsenal. I'm trying
+    to make it a little better during the project, specially using more SOLID
+    principles, but might not be possible due to deadlines. But anyways, it was
+    good fun and good learning.
+
+    document.symbol -> which symbol is being used in a turn, old document.turn variable
+    document.player -> came into place with the necessity of separating display
+    from users, it just shows in the messages who is playing.
+*/
+
+document.symbol = null;
+document.player = null;
+document.winner = null;
+player1 = null;
+player2 = null;
 matches = 0;
 
-function startGame() {
+function startReset() {
     clearBoard();
-    matchCount();
     turns = 1;
-    document.turn = userX;
-    document.winner = null;
-    setMessage(document.turn + "'s turn.");
-}
-
-function matchCount() {
-    if (matches < 1) {
-        setUsername();
-    }
+    matchCount();
+    observer();
     matches++;
 }
 
-function setUsername() {
-    userX = prompt("Player 1: please add your username");
-    userO = prompt("Player 2: please add your username");
+function matchCount() {
+    console.log("Match is " + matches);
+    if (matches < 1) {
+        setUsername();
+    }
+}
 
-    if (userX == null || userO == null)
+function setUsername() {
+    player1 = prompt("Player 1: please add your username");
+    player2 = prompt("Player 2: please add your username");
+    if (player1 == null || player2 == null)
     {
-        userX = "X";
-        userO = "O";
+        player1 = "X";
+        player2 = "O";
     }
 }
 
@@ -33,31 +49,41 @@ function setMessage(message) {
 
 function printSymbol(tile) {
     if (document.winner != null) {
-        setMessage(document.turn + " won. " + "Do you want to play again?");
+        setMessage(document.winner + " won. " + "Do you want to play again?");
     } else if (tile.innerText == '') {
-        tile.innerText = document.turn;
-        turnControl();
-    } else if (document.winner = null && turns < 9) {
-        setMessage("This tile is taken. Still " + document.turn + "'s turn.");
+        tile.innerText = document.symbol;
+        turns++;
+        observer();
+    } else if (document.winner == null && turns < 9) {
+        setMessage("This tile is taken. Still " + document.player + "'s turn.");
     }
 }
 
-function turnControl() {
-    if (winnerCheck(document.turn)) {
-        setMessage("Congratulations! " + document.turn + " won.");
-        document.winner = document.turn;
-    }
-    else if (!winnerCheck(document.turn) && turns >= 9) {
-        setMessage("Wow, much skills, many draws. Do you want to play again?");
-    }
-    else if (document.turn == userX) {
-        document.turn = userO;
-        setMessage(document.turn + "'s turn.");
-        turns++;
+function observer(){
+    console.log("Player is: " + document.player + ". Turn is " + turns + ". Symbol is: " + document.symbol + ". Winner is: " + document.winner);
+    resultDisplay();
+    if (turns % 2 > 0) {
+        document.symbol = "X";
+        document.player = player1;
     } else {
-        document.turn = userX;
-        setMessage(document.turn + "'s turn."); // Needs to be set again so the message switches when an attempt of illegal move is made
-        turns++;
+        document.symbol = "O";
+        document.player = player2;
+    }
+
+    /* The observer checks if there's a winner, if there is, this message won't be
+    printed, but the one from resultDisplay() instead */
+    if (document.winner == null) {
+        setMessage(document.player + "'s turn.");
+    }
+}
+
+function resultDisplay() {
+    if (winnerCheck(document.symbol)) {
+        document.winner = document.player;
+        setMessage("Congratulations! " + document.winner + " won.");
+        console.log("Winner is: " + document.player);
+    } else if (!winnerCheck(document.symbol) && turns >= 9) {
+        setMessage("Wow, much skills, many draws. Do you want to play again?");
     }
 }
 
@@ -91,10 +117,13 @@ function getTile(number) {
     return document.getElementById(number).innerText;
 }
 
-// Erases the board after match is completed
+// Erases the board, turning all of the elements in id from 1 to 9 to empty strings
 function clearBoard() {
     for (var i = 1; i <= 9; i++)
     {
         document.getElementById(i).innerText = "";
     }
+    document.symbol = null;
+    document.player = null;
+    document.winner = null;
 }
